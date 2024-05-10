19e9e57038e3d36b618a03413ad39459a04991a5
Luca Novello <luca@lucanovello.com>
2024-05-09 23:50:50 -0400

    added ws1

---
+++ ws1/ws1-program/main.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+/**
+* Determine if the string s starts with the string prefix.
+* @param s - the string to see if it starts with prefix
+* @param prefix - the prefix to test
+* @returns true if the strings begins with the prefix.
+*/
+int startsWith(const char s[], const char prefix[])
+{
+	char buf[25];
+	int i;
+	int sz = strlen(prefix);
+	if (sz == 0) { return 0; }
+	for (i = 0; i < sz; i++)
+	{
+		buf[i] = s[i];
+	}
+	buf[sz] = '\0';
+	return 0 == strcmp(buf, prefix);
+}
+/**
+* Determine if the string s ends with the string suffix.
+* @param s - the string to see if it ends with suffix
+* @param suffix - the suffix to test
+* @returns true if the strings ends with the suffix.
+*/
+int endsWith(const char s[], const char suffix[])
+{
+	int sz = strlen(suffix);
+	if (sz == 0) { return 0; }
+	int slen = strlen(s);
+	return 0 == strcmp(s + slen - sz, suffix);
+}
+int main(void)
+{
+	char s1[] = { "upended" };
+	char prefix[] = { "" };
+	char suffix[] = { "ed" };
+	printf("%s does %s start with %s\n",
+		s1, startsWith(s1, prefix) ? "" : " not", prefix);
+	printf("%s does %s end with %s\n",
+		s1, endsWith(s1, suffix) ? "" : " not", suffix);
+	return 0;
+}
